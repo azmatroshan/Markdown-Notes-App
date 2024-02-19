5b66c3fcf0e79a102347475fd5e550b0c31314af
Azmat Alam <azmat5star@gmail.com>
2024-02-19 08:46:22 +0530

    first commit

---
+++ src/components/Editor.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react"
+import ReactMde from "react-mde"
+import Showdown from "showdown"
+
+export default function Editor({ currentNote, updateNote }) {
+    const [selectedTab, setSelectedTab] = useState("write")
+
+    const converter = new Showdown.Converter({
+        tables: true,
+        simplifiedAutoLink: true,
+        strikethrough: true,
+        tasklists: true,
+    })  
+
+    // Preserve line breaks in markdown
+    converter.setOption("simpleLineBreaks", true);
+
+    return (
+        <section className="pane editor">
+            <ReactMde
+                value={currentNote.body}
+                onChange={updateNote}
+                selectedTab={selectedTab}
+                onTabChange={setSelectedTab}
+                generateMarkdownPreview={(markdown) =>
+                    Promise.resolve(converter.makeHtml(markdown))
+                }
+                minEditorHeight={80}
+                heightUnits="vh"
+            />
+        </section>
+    )
+}
